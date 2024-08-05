334b6c8a872394ace0fb109d75e403bce1b9a02c
zeusloza <zivlcom@icloud.com>
2024-08-05 13:51:49 -0600

    Pause

--- app/components/Contador.tsx
+++ app/components/Contador.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const Countdown = () => {
   const calculateTimeLeft = () => {
@@ -21,32 +21,45 @@ const Countdown = () => {
   };
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const isPaused = useRef(false);
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
+    if (!isPaused.current) {
+      const timer = setInterval(() => {
+        const newTimeLeft = calculateTimeLeft();
+        setTimeLeft(newTimeLeft);
+        if (Object.keys(newTimeLeft).length === 0) {
+          clearInterval(timer);
+        }
+      }, 1000);
 
-    return () => clearInterval(timer);
+      return () => clearInterval(timer);
+    }
   }, []);
+
+  const pauseCountdown = () => {
+    isPaused.current = true;
+  };
+
+  pauseCountdown();
 
   return (
     <div className="flex flex-col items-center justify-center text-center font-black w-dvw h-[80dvh] text-white px-6">
       <div className="flex justify-center space-x-4">
         <div className="text-center">
-          <span className="block text-6xl ">{timeLeft.days}</span>
+          <span className="block text-6xl">{timeLeft.days || 0}</span>
           <span className="text-gray-300">Días</span>
         </div>
         <div className="text-center">
-          <span className="block text-6xl ">{timeLeft.hours}</span>
+          <span className="block text-6xl">{timeLeft.hours || 0}</span>
           <span className="text-gray-300">Horas</span>
         </div>
         <div className="text-center">
-          <span className="block text-6xl ">{timeLeft.minutes}</span>
+          <span className="block text-6xl">{timeLeft.minutes || 0}</span>
           <span className="text-gray-300">Minutos</span>
         </div>
         <div className="text-center">
-          <span className="block text-6xl ">{timeLeft.seconds}</span>
+          <span className="block text-6xl">{timeLeft.seconds || 0}</span>
           <span className="text-gray-300">Segundos</span>
         </div>
       </div>
